36686ea0ddcc3754ba67cf02292f0ed9046efed8
Tommasin Stefano <79447715+TommasinMartinez@users.noreply.github.com>
2022-01-22 13:26:20 +0100

    Esercizi 01-05

---
+++ 01_variabile/script.js
@@ -0,0 +1,13 @@
+function canPlay() {
+  let personName = 'Paul';
+
+  if (true) {
+    personName = 'Paul';
+  }
+
+  personName += ' plays football';
+
+  console.log(personName);
+}
+
+canPlay();
